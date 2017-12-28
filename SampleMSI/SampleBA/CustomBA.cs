25d76628f7f95d0244c032262502d8eb6be291f6
Ram <karteekyerubandi@gmail.com>
2017-12-28 11:10:24 +0530

    New BA Project

---
+++ SampleMSI/SampleBA/CustomBA.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Tools.WindowsInstallerXml.Bootstrapper;
+using Threading = System.Windows.Threading;
+
+namespace SampleBA
+{
+    public class CustomBA : BootstrapperApplication
+    {
+        public static Model Model { get; private set; }
+
+        public static RootView View { get; private set; }
+
+        public static Threading.Dispatcher Dispatcher { get; private set; }
+
+        protected override void Run()
+        {
+            this.Engine.Log(LogLevel.Standard, "Wix Custom BA started...");
+
+            CustomBA.Dispatcher = Threading.Dispatcher.CurrentDispatcher;
+            
+
+        }
+    }
+}
---
+++ SampleMSI/SampleBA/Model.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Tools.WindowsInstallerXml.Bootstrapper;
+
+namespace SampleBA
+{
+    public class Model
+    {
+
+        private BootstrapperApplication BootstrapperApplication { get; private set; }
+
+        public Model(BootstrapperApplication ba)
+        {
+            this.BootstrapperApplication = ba;
+
+        }
+    }
+}
